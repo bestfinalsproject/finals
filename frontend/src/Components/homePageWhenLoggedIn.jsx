3dfcea3912c591b6f1c3bcb0ded86ef536c0849c
jnguyen3436 <jnguyen3436@gmail.com>
2019-12-10 11:03:05 -0500

    added archive page and statistics

---
+++ frontend/src/Components/homePageWhenLoggedIn.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class homePageWhenLoggedIn extends Component {
+    render() {
+        return (
+            <div>
+                This is the home page when you logged in
+            </div>
+        )
+    }
+}
